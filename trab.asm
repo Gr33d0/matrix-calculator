6c8ad997df540ebb8b89cc4fc0c039808011bea4
Gr33d0 <80392598+Gr33d0@users.noreply.github.com>
2023-06-25 23:42:17 +0100

    Update trab.asm

--- trab.asm
+++ trab.asm
@@ -4,7 +4,6 @@ Matrizes: .space 40 			# 100 bytes para cada matriz, up to 4 matrices
 
 .data
 resultMsg: .asciiz "Valor encontrado no endereço: "
-searchValue: .word 3
 
 menu: .asciiz "Menu:\n1 - Inserir\n2 - Listar\n3 - Apagar\n4 - Multiplicar\n5 - Sair\n\nInsira a opcao: "
 
@@ -360,13 +359,26 @@ IndexInvalido:
     
 
 MultiplicarMatrizes:
+
+    li $t0, 0
+    li $t1, 0		
+    li $t2, 0		
+    li $t3, 0		
+    li $t4, 0		
+    li $t5, 0		
+    li $t6, 0		
+    li $t7, 0		
+    li $t8, 0		
+    li $t9, 0		
+
+
     addi $sp, $sp, -4
     sw $ra, 0($sp)
     jal ListarMatrizes
     lw $ra, 0($sp)
     addi $sp, $sp, 4
 
-    #beq $v1, $0 FimMultiplicar
+    # beq $v1, $0 FimMultiplicar
 
     # Matriz 1
     li $v0, 4
@@ -392,47 +404,14 @@ MultiplicarMatrizes:
     syscall
 
 ProcurarMatriz:
+  la $s0,0x10040000 # Endereço inicial
+  #Primeira matriz
+  la $s1,0x1004000c # Endereço modificavel
+  lw $s2,0($s0) # Numero de elementos
+  addi $s3,$s3,4 # adiciona s3 = 4
+  mul $t2,$s2,$s3 # elementos * 4
+  add $s1,$s1,$t2 # adiciona ao enderenço
+  lw $t3, 0($s1) # Numero de elementos
+  # Para achar qualquer matriz 1º se for por exemplo a segunda vai ter que achar o numero de elementos
 
-  # Carrega o endereço do array
-  li $s0, 0x10040000
-  # Carrega o valor procurado
-  lw $s1, searchValue
-  # Carrega o tamanho do array
-  lw $t2, Matrizes
-  # Inicializa o contador de loop
-  li $t3, 0
-
-loop:
-  # Verifica se o contador atingiu o tamanho do array
-  bge $t3, $t2, end
-
-  # Carrega o valor atual do array
-  lw $t4, ($s0)
-  # Compara o valor atual com o valor procurado
-  beq $t4, $s1, found
-
-  # Incrementa o contador
-  addi $t3, $t3, 1
-  # Incrementa o endereço do array
-  addi $s0, $s0, 4
-  # Volta para o início do loop
-  j loop
-
-found:
-  # O valor foi encontrado
-  # O endereço do valor está em $s0
-
-  # Imprime a mensagem inicial
-  li $v0, 4
-  la $a0, resultMsg
-  syscall
-
-  # Imprime o endereço encontrado
-  li $v0, 1
-  move $a0, $s0
-  syscall
-
-end:
-  # Fim do programa
-  # Coloque aqui o código de término do programa
   
